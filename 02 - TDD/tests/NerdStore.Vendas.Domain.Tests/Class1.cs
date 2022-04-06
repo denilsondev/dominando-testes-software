221d9db39297f2b8468dc480183042e35a2905e2
denilsondev <denilsoncdc10@gmail.com>
2022-04-05 22:58:19 -0300

    classe domain e teste pedido

--- 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NerdStore.Vendas.Domain.Tests
-{
-    public class Class1
-    {
-    }
-}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using NerdStore.Vendas.Domain;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class PedidoTests
+    {
+        [Fact(DisplayName = "Adicionar Item Novo PEdido")]
+        [Trait("Categoria", "Pedido Tests")]
+        public void AdicionarItemPedido_NovoPEdido_DeveAtualizarValor()
+        {
+            //Arrange
+            var pedido = new Pedido();
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            //Act
+            pedido.AdicionarItem(pedidoItem);
+
+            //Assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
+    }
+}
