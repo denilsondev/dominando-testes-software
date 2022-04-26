adef004755bb83ea8c9c644f7e9bb066fb582a15
denilsondev <denilsoncdc10@gmail.com>
2022-04-25 23:08:33 -0300

    adicionando classe adicionar pedido command

---
+++ 02 - TDD/tests/NerdStore.Vendas.Application.Tests/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Linq;
+using NerdStore.Vendas.Application.Commands;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact]
+        public void AdicionarItemPedidoCommand_CommandoEstaValido_DevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(Guid.NewGuid(),
+                Guid.NewGuid(), "Produto Teste", 2, 100);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            //Assert
+            Assert.True(result);
+        }
+
+    }
+}
